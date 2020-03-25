b06267c4a3336268087468b897e65e56bc3d9df3
Liam <liamt.hyatt@gmail.com>
2020-03-24 18:18:00 -0700

    start styling

--- screens/ConvoListScreen.tsx
+++ screens/ConvoListScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity, Image } from 'react-native';
 import { conversations } from '../data/dummyData';
 // import { categories } from '../data/dummyData';
 /* LIAM: Create a list of all ongoing conversations that the user can choose from */
@@ -7,7 +7,10 @@ const ConvoListScreen = ({navigation}) => {
 
   const renderListItem = (itemData) => (
     <TouchableOpacity style={styles.gridItem} onPress={() => {navigation.navigate('Convo')}}>
-      <View><Text>{itemData.item.contact}</Text></View>
+      <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+        <Image style={styles.imageStyle} source={require('../assets/defaultImg.jpg')}></Image>
+        <Text style={styles.textStyle}>{itemData.item.contact}</Text>
+      </View>
     </TouchableOpacity>
   );
 
@@ -20,6 +23,18 @@ const ConvoListScreen = ({navigation}) => {
 
 const styles = StyleSheet.create(
   {
+    imageStyle: {
+      height: 45,
+      width: 45,
+      margin: 25,
+    },
+    textStyle: {
+      color: 'white',
+      fontWeight: 'bold',
+      textAlign: 'right',
+      margin: 25,
+      fontSize: 35,
+    },
     screen: {
       flex: 1,
       justifyContent: 'center',
@@ -29,7 +44,7 @@ const styles = StyleSheet.create(
       backgroundColor: 'grey',
       flex: 1,
       margin: 15,
-      height: 150
+      height: 100
     },
   }
 );
