0ea41e3c2aaafaf18459310e11425d297911e4eb
Liam <liamt.hyatt@gmail.com>
2020-04-07 10:35:26 -0700

    first test with onSuccess of qr

--- screens/NewConvoScreen.tsx
+++ screens/NewConvoScreen.tsx
@@ -1,19 +1,42 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Linking } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Linking, TextInput } from 'react-native';
 import QRCode from 'react-native-qrcode-svg'
 import QRCodeScanner from 'react-native-qrcode-scanner';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const NewConvoScreen = ({}) => {
+  const [name, setName] = useState("")
+
+  var handleName = (enteredName) => {
+    setName(enteredName)
+    console.log(name);
+  }
+
   const key = {
     "_id": "pCs9SGdPvAS59NmtnzuGVImTn132",
-    "publicKey": "-----BEGIN PUBLIC KEY-----MIIBITANBgkqhkiG9w0BAQEFAAOCAQ4AMIIBCQKCAQByDYC+/YyKYQplZ17iAyMq1XGLOMIcfSlKQCPGkIZWqrQsgGlTlRNeJHgbwpcR15S1qtwkWszeaanrB1h4BQaVV3yf8wlerui+1p+Yv24ZyUaRCyzTPjwaedFJ6iVYP0UA0iBrZUWfeWxcNimM+AWGQioKFhdD7BvIzvn9LBOt0x571D2v7K2LmWZPvLgE/gvwLT85gvcENQ3hamGpFaZuupeWvzHUeChS8mz2/fmEX9RTV1OSwLmWb438x6pLJ0OuZlUtJWpoxS0tVlnIzBIyCE98a0hj+a6rVLevrtxoSwrL+/TaQuU41nB3TfiPenyStJa/f2grB8UAwqb8w4pJAgMBAAE=-----END PUBLIC KEY-----" 
+    "publicKey": `-----BEGIN PUBLIC KEY-----
+    MIIBITANBgkqhkiG9w0BAQEFAAOCAQ4AMIIBCQKCAQByDYC+/YyKYQplZ17iAyMq
+    1XGLOMIcfSlKQCPGkIZWqrQsgGlTlRNeJHgbwpcR15S1qtwkWszeaanrB1h4BQaV
+    V3yf8wlerui+1p+Yv24ZyUaRCyzTPjwaedFJ6iVYP0UA0iBrZUWfeWxcNimM+AWG
+    QioKFhdD7BvIzvn9LBOt0x571D2v7K2LmWZPvLgE/gvwLT85gvcENQ3hamGpFaZu
+    upeWvzHUeChS8mz2/fmEX9RTV1OSwLmWb438x6pLJ0OuZlUtJWpoxS0tVlnIzBIy
+    CE98a0hj+a6rVLevrtxoSwrL+/TaQuU41nB3TfiPenyStJa/f2grB8UAwqb8w4pJ
+    AgMBAAE=
+    -----END PUBLIC KEY-----`
   }
-  const stringKey = JSON.stringify({key}).slice(7,-1)
 
-  const onSuccess = (e) => {
-    Linking.openURL(e.data).catch(err =>
-      console.error('An error occured', err)  
-    );
+  const stringKey = JSON.stringify(key);
+
+  const onSuccess = async (e) => {
+    try {
+      var e = JSON.parse(e);
+      e.name = name;
+      e = JSON.stringify(e);
+      await AsyncStorage.setItem(e.data._id,e.data);
+      console.log(e.data);
+    } catch (e) {
+      console.log("Key did not save" + e)
+    }
   };
 
   return(
@@ -29,7 +52,7 @@ const NewConvoScreen = ({}) => {
           onRead={onSuccess}
           cameraStyle={{ height: 200, marginTop: 20, width: 200, alignSelf: 'center', justifyContent: 'center' }}
         />
-      
+        <TextInput value={name} placeholder="something" onChangeText={name => handleName(name)} />
       </View>
     </>
   );
